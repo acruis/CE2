9abd18a7a108a0871708f05cf126b0a6bb11d85e
acruis <datsquall@gmail.com>
2014-09-17 01:06:22 +0800

    Migration from CE1

---
+++ TextBuddyUnits/unittest1.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "TextBuddy.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TextBuddyUnits
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
